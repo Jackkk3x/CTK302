398209cc691013674c076dde45968b915a2901b6
Jackkk3x <kmjack6@gmail.com>
2020-09-01 11:13:08 -0500

    Update sketch.js

--- p5/Unit1/scene/sketch.js
+++ p5/Unit1/scene/sketch.js
@@ -1,21 +1,22 @@
-function setup() {
+ function setup() {
   createCanvas(500, 500);
-  
+
 }
 
 function draw() {
-  background(100) ; 
+
+  if(mouseIsPressed){
+    background('blue');
+  } else{
+  background('grey') ;
+  }
   // put your shapes here!
-  
-  
+ellipse(100,100,100,100)
+
   text(mouseX + " , " + mouseY, 10, 20) ;  // this helps know where the mouse is
-} 
+}
 
-  
-
-function mouseReleased() { 
+function mouseReleased() {
   // if you click the mouse, you'll see the location in the debugging console
-  print(mouseX + " , " + mouseY) ; 
-} 
-
- 
+  print(mouseX + " , " + mouseY) ;
+}
