a6c4582620bd4b80691383149693e1f7838cdaff
Jackkk3x <kmjack6@gmail.com>
2020-09-06 23:50:53 -0500

    i

--- p5/Unit1/fonts/sketch.js
+++ p5/Unit1/fonts/sketch.js
@@ -1,6 +1,5 @@
-var f, f1 ;
-
-
+var f,
+ f1 ;
 
 function setup() {
   createCanvas(800,800);
@@ -8,18 +7,15 @@ function setup() {
   f = loadFont("cookie_2/Cookie.ttf");
     f1 = loadFont("cookie_2/Cookie.ttf");
 
-   textAlign(CENTER)
+   textAlign(CENTER);
 }
 
 function draw() {
   background(100);
 
+fill('green');
   textFont(f);
   textSize(80);
-  text("Everything", width / 2, height / 2)
-
-  textFont(f1);
-  textSize(160);
-  text("Happens for a reason", width / 2, height / 2)
+  text("Everything happens for a reason", width / 2, height / 2 + 80);
 
 }
