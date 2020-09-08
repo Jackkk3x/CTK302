1eaa1bc0b3accbd8cc2b2b38cf39974c4b5048eb
Jackkk3x <kmjack6@gmail.com>
2020-09-08 11:32:33 -0500

    rect

---
+++ p5/Unit1/rect/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  fill(random(255),random(255),random(255),random(255));
+rect(mouseX,mouseY,random(50,150);random(50,150);
+}
+
+function mouseReleased(){
+
+  saveCanvas('myPicture','png');
+  background(100);
+}
