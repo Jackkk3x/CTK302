643f6797a169ae2310b9445a0910dd2bca6d0b44
Jackkk3x <kmjack6@gmail.com>
2020-09-22 11:21:28 -0500

    o

--- p5/Unit2/button/sketch.js
+++ p5/Unit2/button/sketch.js
@@ -1,26 +1,34 @@
 let myState = 0 ;
 function setup() {
   createCanvas(500, 500)
-  textAlign(CENTER)
-  textSize(32);
+
 }
 
 function draw() {
-  switch (state) {
+  switch(myState) {
     case 0:
-      background('purple');
-      text("why did the baby\ncross the road?", 250, 250);
+      background('white');
+      text("case 0", 250, 250);
+      break;
+
     case 1:
-      background('orange');
-      text("because it was|n stapled to the chicken", 250, 250)
+      background('grey');
+      text("case1", 250, 250);
+      break;
+
+      case 2:
+      background('pink');
+      text("case 2", 250, 250);
+      break;
+
   }
 
 
 }
 
 function mouseReleased() {
-  state++;
-  if (state > 1) {
-    state = 0;
+  myState++;
+  if (myState > 1) {
+    myState = 0;
   }
 }
