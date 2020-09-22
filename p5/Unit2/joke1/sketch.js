afa443a6ffdf878df4818db2b94f109781b6b94c
Jackkk3x <kmjack6@gmail.com>
2020-09-22 11:10:48 -0500

    tweak

--- p5/Unit2/joke1/sketch.js
+++ p5/Unit2/joke1/sketch.js
@@ -1,10 +1,25 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(500,500)
+  textAlign(CENTER)
+  textSize(32);
 }
 
 function draw() {
+  switch(state){
+    case 0:
+    background('purple');
+  text("why did the baby\ncross the road?" , 250,250);
+  case 1:
+  background('orange');
+  text("because it was|n stapled to the chicken" , 250,250)
+  }
 
 
 }
 
+function mouseReleased(){
+  state++;
+  if(state > 1){
+    state = 0 ;
+  }
 }
