6d7c9ebe171ee74550b0ea3f878513b13f3f77fe
Jackkk3x <kmjack6@gmail.com>
2020-09-22 11:00:41 -0500

    8/22

--- p5/Unit2/joke2/sketch.js
+++ p5/Unit2/joke2/sketch.js
@@ -1,21 +1,50 @@
-let timer = 0 ; 
-let state = 0 ;  
-function setup() { 
-  createCanvas(500, 500); 
+let timer = 0;
+
+let state = 0;
+
+
+function setup() {
+
+  createCanvas(500, 500);
+
   textSize(32)
   textAlign(CENTER)
   rectmode(CENTER)
-}  
-function draw() {    switch(state)
-  {   case 0: 
-  background('red') ; 
- text("why did the chicken cross the road",250,250, 350,200) ; 
-   timer = timer + 1 ; 
-    if (timer > 5*60) { 
-      state = 1 ; 
-       timer = 0 ; 
-      }   
+}
 
-  break ;  
-   case 1:     background('blue') ; 
-   text("to get to the other side", 250, 250) ;     break ;    }  } 
+
+function draw() {
+
+  switch (state) {
+
+    case 0:
+
+      background('red');
+
+      text("why did the chicken cross the road", 250, 250, 350, 200);
+
+      timer = timer + 1;
+
+      if (timer > 5 * 60) {
+
+        state = 1;
+
+        timer = 0;
+
+      }
+
+
+
+
+      break;
+
+
+    case 1:
+      background('blue');
+
+      text("to get to the other side", 250, 250);
+      break;
+
+  }
+
+}
