3744724e5f97d5f47293750cf281b641132c1a79
Jackkk3x <kmjack6@gmail.com>
2020-09-27 18:49:22 -0500

    l

--- p5/Unit2/joke1/sketch.js
+++ p5/Unit2/joke1/sketch.js
@@ -10,7 +10,7 @@ function draw() {
   switch (state) {
     case 0:
       background('purple');
-      text("why did the baby\ncross the road?", 250, 250);
+      text("why did the baby\n cross the road?", 250, 250);
     case 1:
       background('orange');
       text("because it was|n stapled to the chicken", 250, 250)
