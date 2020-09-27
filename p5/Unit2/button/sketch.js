9277bf82d266292498eefd2879b831d7d375fcae
Jackkk3x <kmjack6@gmail.com>
2020-09-27 17:43:40 -0500

    yo

--- p5/Unit2/button/sketch.js
+++ p5/Unit2/button/sketch.js
@@ -1,11 +1,12 @@
-let myState = 0 ;
+let myState = 0;
+
 function setup() {
   createCanvas(500, 500)
 
 }
 
 function draw() {
-  switch(myState) {
+  switch (myState) {
     case 0:
       background('white');
       text("case 0", 250, 250);
@@ -16,7 +17,7 @@ function draw() {
       text("case1", 250, 250);
       break;
 
-      case 2:
+    case 2:
       background('pink');
       text("case 2", 250, 250);
       break;
@@ -28,8 +29,7 @@ function draw() {
 
 function mouseReleased() {
   myState++;
-  if((mouseX > 10 && (mouseX <60) && (mouseY > 10) && (mouseY < 35))
-  if (myState > 2) {
-    myState = 0;
+  if ((mouseX > 10 && (mouseX < 60) && (mouseY > 10) && (mouseY < 35)) if (myState > 2) {
+      myState = 0;
+    }
   }
-}
