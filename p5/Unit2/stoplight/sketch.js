d6f7eee901ee90d4ac30bbf673d1d357556c2fac
Jackkk3x <kmjack6@gmail.com>
2020-10-12 23:58:38 -0500

    vb

--- p5/Unit2/stoplight/sketch.js
+++ p5/Unit2/stoplight/sketch.js
@@ -1,5 +1,7 @@
 let state = 0;
 let timer = 0;
+let x = 0;
+let velocity = 1 ;
 function setup() {
   createCanvas(800, 800);
   ellipseMode(CENTER);
@@ -10,6 +12,14 @@ function draw() {
   background(100);
   fill('yellow');
 rect(width/2, height/2, 200, 600);
+
+//carcode
+fill('blue') ;
+rect(0, 750, 75 ,20);
+x=x + velocity;
+if(x > width){
+  x = 0;
+}
 
 
 
@@ -22,6 +32,7 @@ ellipse(width/2, height/2-170,150,150);
 fill('grey');
 ellipse(width/2, height/2,150,150);
 ellipse(width/2,height/2+170,150,150);
+velocity = 0;
 
 break;
 
@@ -32,6 +43,7 @@ ellipse(width/2, height/2-170,150,150);
 ellipse(width/2, height/2,150,150);
 fill('green');
 ellipse(width/2,height/2+170,150,150);
+velocity = 10;
 
 break;
 
@@ -43,6 +55,7 @@ fill('yellow');
 ellipse(width/2, height/2,150,150);
 fill('grey') ;
 ellipse(width/2,height/2+170,150,150);
+velocity = 5;
 
 break;
 }
