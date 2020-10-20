d16c1c9f9f26b01f953933c5f110cfef078e6304
Jackkk3x <kmjack6@gmail.com>
2020-10-20 11:28:13 -0500

    b

--- p5/Unit3/vote/sketch.js
+++ p5/Unit3/vote/sketch.js
@@ -1,23 +1,30 @@
 let cars = [];
+let f1, f2, f3;
+let bg;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Spawn 20 objects
-for(let i = 0;i < 20; i++){
-cars.push(new Car());
+f1= loadFont("assets/KGChasingCars.ttf");
+f2= loadFont("assets/rock.ttf");
+f3= loadFont("assets/spaceage.ttf");
+bg= loadImage("assets/fallPic.jpg");
 
-}
+  // Spawn 20 objects
+  for (let i = 0; i < 20; i++) {
+    cars.push(new Car());
+
+  }
 }
 
 function draw() {
   background(100);
 
   // display and move 20 objects
-for(let i = 0;i < 20; i++){
-cars[i].display();
-cars[i].move();
-}
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].move();
+  }
 }
 
 
@@ -28,15 +35,21 @@ class Car {
 
   constructor() {
     this.pos = createVector(100, 100);
-    this.vel = createVector(random(-3, 3), random(-3, 3));
-
+    this.vel = createVector(random(-8, 8), random(-8, 8));
+    this.size= random(40,240);
+    this.c= color(random(150,200),random(50), random(50));
+    this.font= f1;
   }
 
   // methods
 
 
   display() {
-    rect(this.pos.x, this.pos.y, 50, 25);
+    //rect(this.pos.x, this.pos.y, 50, 25);
+    fill(this.c);
+    textFont(this.font);
+    textSize(this.size);
+    text("vote",this.pos.x, this.pos.y);
   }
 
   move() {
